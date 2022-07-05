1a8ab34208840dcfaf851f235171032015cc8907
Derek <dereklau15@gmail.com>
2022-07-05 22:24:25 +0900

    Add checkInput function to output button values

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operand1, operand2 ) {
-    let operator = ["+","-","/","*"];
+function operate(operand1, operand2) {
+    operator = ["+","-","/","*"];
     if (operator === "+") {
         add(operand1, operand2);
     } else if (operator === "-") {
@@ -32,20 +32,32 @@ function operate(operand1, operand2 ) {
     }
 }
 
-function inputNumber() {
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach((button) => button.addEventListener('click', () => {
-        if (button.value) {
-            firstNumber = button.value;
-            displayValue = firstNumber;
-        }
-    })
-    );
-}
-
 function populateDisplay() {
     const display = document.querySelector(".calculator-display");
     display.value = displayValue;
 }
 
 populateDisplay();
+
+function checkInput() {
+    buttons = document.querySelector(".calculator-buttons");
+    buttons.addEventListener('click', (event) => {
+        const target = event.target;
+        if (!target.matches("button")) {
+            return;
+        } else if (target.classList.contains("operator")) {
+            console.log("operator", target.value);
+            return;
+        } else if (target.classList.contains("decimal-button")) {
+            console.log("decimal", target.value);
+            return;
+        } else if (target.classList.contains("clear-button")) {
+            console.log("clear", target.value);
+            return;
+        } else {
+        console.log('digit', target.value);
+        }
+    });
+}
+
+checkInput();
